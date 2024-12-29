a4c0b0211773de9db9c15d61ab1dd17ae6dd26b6
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-12-29 19:03:40 +0530

    port added

--- server/index.js
+++ server/index.js
@@ -8,18 +8,17 @@ import authroutes from "./routers/auth.route.js"
 
 dotenv.config();  // it reads the.env file and sets the environment variables.
 
-const app = express();
-
-
+const app  = express();
+const PORT = process.env.PORT ||5000;
 
 app.get('/', async (req, res) => {
     res.send(" hello serever is ready to paly");
 })
 
-app.use("/api/auth",authroutes);
- // pass like const 
+app.use("/api/auth",authroutes); // pass like function
+ 
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     connectDB();
-    console.log("server is running on port 3000");
+    console.log("server is running on port 3000",PORT);
 });
