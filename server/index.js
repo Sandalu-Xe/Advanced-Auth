f5652d02343423376ff188233d0cad480ae0ae21
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-12-29 13:59:42 +0530

    worked

--- server/index.js
+++ server/index.js
@@ -2,4 +2,10 @@ import express  from 'express'; //module (type)
 
 const app =express();
 
-app.use
+app.get('/book', async (req, res) => {
+    res.send(" hello serever is ready to paly");
+})
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+});
