255783f1b524e0b92f11d87d552a75d73336b0ec
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-12-29 21:04:32 +0530

    set up post

--- server/routers/auth.route.js
+++ server/routers/auth.route.js
@@ -1,15 +1,12 @@
 import express  from 'express';
-
 const router = express.Router();
-
 import {signup,login,logout} from "../controller/auth.controller.js";
 
 
+router.post("/signup",signup);
 
-router.get("/signup",signup);
+router.post("/login",login);
 
-router.get("/login",login);
-
-router.get("/logout",logout);
+router.post("/logout",logout);
 
 export default router;
