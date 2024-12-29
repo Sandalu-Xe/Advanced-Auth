b7f981e541262fa694dbf1a2790a4ae3c649e17e
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-12-29 16:09:55 +0530

    add router

---
+++ server/routers/auth.route.js
@@ -0,0 +1,15 @@
+import express  from 'express';
+
+const router = express.router();
+
+router.get("/signup", (req, res) => {
+    res.render("signup route");
+});
+router.get("/signup", (req, res) => {
+    res.render("signup route");
+});
+router.get("/signup", (req, res) => {
+    res.render("signup route");
+});
+
+export default router;
