a7ea9f7f6f4eb0f314525310e9286731d35088c1
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-12-30 19:25:23 +0530

    add nodemail

---
+++ server/mailtrap/mailtrap.config.js
@@ -0,0 +1,27 @@
+const { MailtrapClient } = require("mailtrap");
+
+const TOKEN = "701ae8d26822cb06884d41daafd5d4de";
+
+const client = new MailtrapClient({
+  token: TOKEN,
+});
+
+const sender = {
+  email: "hello@demomailtrap.com",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  {
+    email: "sandaluthushan20@gmail.com",
+  }
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
