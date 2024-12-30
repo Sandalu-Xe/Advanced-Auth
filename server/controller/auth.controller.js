bff216b46721e5de8e85fd53705f613e9d68ce5b
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-12-30 23:04:14 +0530

    verify defined

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -9,7 +9,7 @@ import {
 } from "../mailtrap/emails.js";
 
 
-// Signup
+// Signup conroller
 export const signup = async (req, res) => {
 
 	const { email, password, name } = req.body;
@@ -59,7 +59,41 @@ export const signup = async (req, res) => {
 	}
 };
 
-// login
+
+export const verifyEmail = async (req, res) => {
+	const { code } = req.body;
+	try {
+		const user = await User.findOne({
+			verificationToken: code,
+			verificationTokenExpiresAt: { $gt: Date.now() },
+		});
+
+		if (!user) {
+			return res.status(400).json({ success: false, message: "Invalid or expired verification code" });
+		}
+
+		user.isVerified = true;
+		user.verificationToken = undefined;
+		user.verificationTokenExpiresAt = undefined;
+		await user.save();
+
+		await sendWelcomeEmail(user.email, user.name);
+
+		res.status(200).json({
+			success: true,
+			message: "Email verified successfully",
+			user: {
+				...user._doc,
+				password: undefined,
+			},
+		});
+	} catch (error) {
+		console.log("error in verifyEmail ", error);
+		res.status(500).json({ success: false, message: "Server error" });
+	}
+};
+
+// login controller
 
 export const login = async (
     req, res, next) => { 
@@ -67,7 +101,7 @@ export const login = async (
 }
 
 
-// logout
+// logout controller
 export const logout = async (
     req, res, next) => {
      res.send("logout  routers")
--- server/routers/auth.route.js
+++ server/routers/auth.route.js
@@ -1,12 +1,14 @@
 import express  from 'express';
 const router = express.Router();
-import {signup,login,logout} from "../controller/auth.controller.js";
-
+import {signup,login,logout,verifyEmail,} from "../controller/auth.controller.js";
 
 router.post("/signup",signup);
 
 router.post("/login",login);
 
 router.post("/logout",logout);
+
+router.post("/verify-email", verifyEmail);
+
 
 export default router;
