f324063974cfc362a718df7b09a066ed4f2e965a
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-12-31 19:53:05 +0530

    login and signup done

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import FloatingShape from './components/FloatingShape';
+import SignUpPage from './pages/SignUpPage.jsx';
+import LoginPage from './pages/SignUpPage.jsx';
 
 function App() {
   return (
