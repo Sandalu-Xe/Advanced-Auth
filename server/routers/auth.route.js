1e9bb04988e973d117d2386d367453719e2b293f
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-12-31 13:47:48 +0530

    jwt save

--- server/routers/auth.route.js
+++ server/routers/auth.route.js
@@ -1,10 +1,16 @@
 import express  from 'express';
 const router = express.Router();
-import {signup,login,logout,verifyEmail,forgotPassword,resetPassword} from "../controller/auth.controller.js";
+import {
+    signup,
+    login,
+    logout,
+    verifyEmail,
+    forgotPassword,
+    resetPassword,
+    checkAuth,
+} from "../controller/auth.controller.js";
 
 import { verifyToken } from "../Middleware/verifyToken.js";
-
-
 
 router.get("/check-auth", verifyToken, checkAuth);
 
@@ -21,3 +27,4 @@ router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
 
 export default router;
+
