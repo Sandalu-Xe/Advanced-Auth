3c2d95c8cbffd5a699056297bc439c12d6c11646
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2025-01-01 15:48:59 +0530

    upadete cores

--- Client/src/pages/SignUpPage.jsx
+++ Client/src/pages/SignUpPage.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
 import { useState } from "react";
-import {  Lock, Mail, User } from "lucide-react";
+import {  Lock, Mail, User,Loader} from "lucide-react";
 import { motion } from "framer-motion";
 import Input from "../components/Input";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import PasswordStrengthMeter from "../components/PasswordStrengthMeter";
+import { useAuthStore } from "../store/authStore";
 
 const SignUpPage = () => {
 	const [name, setName] = useState('');
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const navigate = useNavigate();
 
-	const handleSignUp = (e) => {
-	  e.preventDefault(); 
-	
-	}
+	const { signup, error, isLoading } = useAuthStore();
+
+	const handleSignUp = async (e) => {
+		e.preventDefault();
+
+		try {
+			await signup(email, password, name);
+			navigate("/verify-email");
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
   return (
 
@@ -62,9 +72,12 @@ const SignUpPage = () => {
 						whileHover={{ scale: 1.02 }}
 						whileTap={{ scale: 0.98 }}
 						type='submit'
+						disabled={isLoading}
 					
 					>
+						{isLoading ? <Loader className=' animate-spin mx-auto' size={24} /> : "Sign Up"}
 					</motion.button>
+					{error && <p className='text-red-500 font-semibold mt-2'>{error}</p>}
 					<PasswordStrengthMeter password={password} />
 				
 				</form>
--- server/index.js
+++ server/index.js
@@ -4,13 +4,15 @@ import dotenv from "dotenv";
 import {connectDB} from '../server/DB/connectDB.js';
 import authroutes from "./routers/auth.route.js"
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 
 
 dotenv.config();  // it reads the.env file and sets the environment variables.
 
 const app  = express();
 const PORT = process.env.PORT ||5001;
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
