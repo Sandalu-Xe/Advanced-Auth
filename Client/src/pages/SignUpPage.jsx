e484b15ddc18363e3f1e4f3f4206c7c1f48671af
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2025-01-01 13:09:36 +0530

    passord strenth

--- Client/src/pages/SignUpPage.jsx
+++ Client/src/pages/SignUpPage.jsx
@@ -16,7 +16,7 @@ const SignUpPage = () => {
 	}
 
   return (
-	
+
 	
 <motion.div
 			initial={{ opacity: 0, y: 20 }}
@@ -53,6 +53,17 @@ const SignUpPage = () => {
 						value={password}
 						onChange={(e) => setPassword(e.target.value)}
 					/>
+					<motion.button
+						className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
+						font-bold rounded-lg shadow-lg hover:from-green-600
+						hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
+						 focus:ring-offset-gray-900 transition duration-200'
+						whileHover={{ scale: 1.02 }}
+						whileTap={{ scale: 0.98 }}
+						type='submit'
+					
+					>
+					</motion.button>
 				
 				</form>
 			</div>
