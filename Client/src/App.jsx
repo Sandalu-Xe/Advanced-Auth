5e1e72badc139bc43b3aff851e4e2477c5b02f79
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2025-01-02 00:21:33 +0530

    reset password done

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,7 +6,7 @@ import LoginPage from "./pages/LoginPage";
 import EmailVerificationPage from "./pages/EmailVerificationPage";
 import DashboardPage from "./pages/DashboardPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
-import ResetPasswordPage from "./pages/ResetPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage.jsx";
 
 import LoadingSpinner from "./components/LoadingSpinner";
 
