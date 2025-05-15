3e2b17d5aa8af6548a4a0323de0e553fe3055aca
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2025-05-15 10:14:50 +0530

    ui changed

--- server/routers/auth.route.js
+++ server/routers/auth.route.js
@@ -15,7 +15,7 @@ import { verifyToken } from "../Middleware/verifyToken.js";
 router.get("/check-auth", verifyToken, checkAuth);
 
 router.post("/signup",signup);
-
+``
 router.post("/login",login);
 
 router.post("/logout",logout);
